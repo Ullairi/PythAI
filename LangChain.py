3df69dbd39bfd261908473fbc7abd73639e8dd35
Ullairi <maks.tih2005@gmail.com>
2025-11-03 15:39:58 +0100

    updated comments

--- LangChain.py
+++ LangChain.py
@@ -12,17 +12,22 @@ api_key = os.getenv("GEMINI_API_KEY")
 if not api_key:
     raise ValueError("NO API KEY FOUND")
 
+# Загружаем документы с веб-страницы
 url = "https://de.wikipedia.org/wiki/Wikipedia:Hauptseite"
 loader = WebBaseLoader(url)
 docs = loader.load()
 
+# Инициализируем генеративную модель Google
 llm = ChatGoogleGenerativeAI(model="gemini-pro")
 
+# Создаем шаблон для промптов,
 prompt = ChatPromptTemplate.from_template("Text short summatize :\n\n{context}")
 
+# создаем цепочку которая объединяет документы
 chain = create_stuff_documents_chain(llm, prompt)
 
 summary = chain.invoke({"context": docs})
 
+# Выводим полученный результат
 print(" Web-page summary:")
 print(summary)
